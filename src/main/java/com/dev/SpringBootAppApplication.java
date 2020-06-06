23eadbe399e283fb6c5fc51c34eca98326d9c9ae
Krushna jalgamwar <krushnajalgamwar@gmail.com>
2020-06-06 18:23:17 -0400

    Servlet initializer using in main class

--- src/main/java/com/dev/SpringBootAppApplication.java
+++ src/main/java/com/dev/SpringBootAppApplication.java
@@ -2,12 +2,20 @@ package com.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootAppApplication {
+public class SpringBootAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppApplication.class, args);
+		System.out.println("Hello    world");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootAppApplication.class);
 	}
 
 }
